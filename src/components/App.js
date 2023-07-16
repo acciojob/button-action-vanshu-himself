a4b0fdeb7e80c4fa0476a711797e9e36180fc16f
Vansh Gupta <you@example.com>
2023-07-16 19:23:33 +0530

    1st try

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+ 
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click"  onClick={()=> document.getElementById('para').className="show"}>submit</button>
+      
+
     </div>
   );
 }
